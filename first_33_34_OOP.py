df07cb81f20f7228c8ee48dcdb9c99f823dadde9
Stanislavest <Stanislavest@yandex.ru>
2022-01-29 19:01:07 +0500

    1~

--- first_33_34_OOP.py
+++ first_33_34_OOP.py
@@ -262,30 +262,303 @@
 # ==========================================================
 
 
-class MyDecorator:
-    def __init__(self, arg):
-        self.name = arg
+# class MyDecorator:
+#     def __init__(self, arg):
+#         self.name = arg
+#
+#     def __call__(self, func):
+#         def wrap(x, y):
+#             print('перед вызовом функции')
+#             print(self.name)
+#             func(x, y)  # функция приходит как аргумент
+#             print('после вызова функции')
+#         return wrap
+#
+#
+# @MyDecorator("test2")
+# def function(a, b):
+#     print(a, b)
+#
+#
+# function(2, 5)
 
-    def __call__(self, func):
-        def wrap(x, y):
-            print('перед вызовом функции')
-            print(self.name)
-            func(x, y)
-            print('после вызова функции')
-        return wrap
+# ========================================================== 27.01.2022
+# ========================================================== ДЕКОРАТОРЫ
+
+# class Power:
+#     def __init__(self, arg):
+#         self.arg = arg
+#
+#     def __call__(self, func):
+#         def wrapper(a, b):
+#             res = func(a, b)
+#             return res ** self.arg
+#         return wrapper
+#
+# @Power(12)
+# def multuple(a, b):
+#     return a * b
+#
+# print("Result:", multuple(2, 2))
+
+# ===============================  декорирование методов внутри класса
+
+# def dec(fn):
+#     def wrap(*args, **kwargs):
+#         print("*" * 20)
+#         fn(*args, **kwargs)
+#         print("*" * 20)
+#     return wrap
+#
+#
+# class Person:
+#     def __init__(self, name, surname):
+#         self.name = name
+#         self.surname = surname
+#
+#     @dec
+#     def info(self):
+#         print(f'{self.name} {self.surname}')
+#
+#
+# p1 = Person("Виталий", "Карасев")
+# p1.info()
 
 
-@MyDecorator("test2")
-def function(a, b):
-    print(a, b)
+# ========================================================== понятно!
+
+# def decorator(cls):
+#     class Wrapper(cls):
+#
+#         def doubler(self, value1):
+#             return value1 * 2
+#
+#     return Wrapper
+#
+# @decorator
+# class ActualClass:
+#     def __init__(self):
+#         print("Init ActualClass")
+#
+#     def quad(self, value):
+#         return value * 4
+#
+#
+# obj = ActualClass()
+# print(obj.quad(4))
+# print(obj.doubler(4))
 
 
-function(2, 5)
+# ==========================================================
+
+# class Message:
+#     _REGISTERY = {}
+#
+#     def __init__(self, text):
+#         self.text = text
+#
+#     @classmethod
+#     def register(cls, name):
+#         def decorator(klass):
+#             cls._REGISTERY[name] = klass
+#             return klass
+#         return decorator
+#
+#     @classmethod
+#     def create(cls, message_type, text):
+#         klass = cls._REGISTERY.get(message_type)
+#         if klass is None:
+#             raise ValueError("Такого мессенджера нет")
+#         print(text, end=" ")
+#         return klass(text)
+#
+#
+# @Message.register("Telegram")
+# class TelegramMessage(Message):
+#     def send(self):
+#         print("(Telegram)")
+#
+# @Message.register("WhatsApp")
+# class WhatsAppMessage(Message):
+#     def send(self):
+#         print("(WhatsApp)")
+#
+# @Message.register("Viber")
+# class WhatsAppMessage(Message):
+#     def send(self):
+#         print("(Viber)")
+#
+#
+# m1 = Message.create("Telegram", "text")
+# m1.send()
+# m2 = Message.create("WhatsApp", "new text")
+# m2.send()
+# m3 = Message.create("WhatsApp", "text text text")
+# m3.send()
+# m4 = Message.create("Viber", "text text text text")
+# m4.send()
+
+# =====================================================================
+# ======================== ДЕСКРИПТОР атрибут класса являющийся классом
+
+# class String:
+#     def __init__(self, value=None):
+#         if value:
+#             self.set(value)
+#
+#     def set(self, value):
+#         self.__value = value
+#
+#     def get(self):
+#         return self.__value
+#
+#
+# class Person:
+#     def __init__(self, name, surname):
+#         self.name = String(name)
+#         self.surname = String(surname)
+#
+#     # @property
+#     # def name(self):
+#     #     return self.__name
+#     #
+#     # @name.setter
+#     # def name(self, value):
+#     #     self.__name = value
+#     #
+#     # @property
+#     # def surname(self):
+#     #     return self.__surname
+#     #
+#     # @surname.setter
+#     # def surname(self, value):
+#     #     self.__surname = value
+#
+#
+# p = Person("Иван", "Иванович")
+# print(p.name.get())
+# p.name.set("Игорь")
+# print(p.name.get())
+
+# =====================================================================
+# __get__() прочитать значение
+# __set__() установить значение
+# __delete__()
+# __set_name__()
+
+# class ValidString:
+#     def __set_name__(self, owner, name):
+#         self.__name = name
+#
+#     def __get__(self, instance, owner):
+#         return instance.__dict__[self.__name]
+#
+#     def __set__(self, instance, value):
+#         if not isinstance(value, str):
+#             raise ValueError(f'{self.__name} должно быть строкой')
+#         instance.__dict__[self.__name] = value
+#
+#
+# class Person:
+#     name = ValidString()
+#     surname = ValidString()
+#
+#     def __init__(self, name, surname):
+#         self.name = name
+#         self.surname = surname
+#
+#
+# p = Person("Иван", "Иванович")
+# print(p.name)
+# print(p.surname)
+# p.surname = "Игорь"
+# print(p.name)
+# print(p.surname)
+
+# ============================================================== задача
+# non data descriptor - дескриптор не данных (только __get__)
+# data descriptor - дескриптор данных
+
+# class NonNegative:
+#     def __set_name__(self, owner, name):
+#         self.__name = name
+#
+#     def __set__(self, instance, value):
+#         if value < 0:
+#             raise ValueError("Значение должно быть положительным")
+#         instance.__dict__[self.__name] = value
+#
+#     def __get__(self, instance, owner):
+#         return instance.__dict__[self.__name]
+#
+#
+# class Order:
+#     prise = NonNegative()
+#     quantity = NonNegative()
+#
+#     def __init__(self, name, price, quantity):
+#         self.name = name
+#         self.price = price
+#         self.quantity = quantity
+#
+#     def total(self):
+#         return self.price * self.quantity
+#
+#
+# apple = Order('apple', 5, 10)
+# print(apple.total())
+# apple = Order('apple', 5, -10)
+
+# ============================================================== МЕТАКЛАССЫ
+#
+
+# class MyList(list):
+#     def get_length(self):
+#         return len(self)
+#
+#
+# lst = MyList()
+# lst.append(1)
+# lst.append(2)
+# lst[0] = 3
+# print(lst, lst.get_length())
+
+# ===================================================== =
+
+# MyList = type(
+#     'MyList',  # имя
+#     (list,),  # от чего наследуется
+#     dict(get_length=lambda self: len(self))  #
+# )
+#
+# lst = MyList()
+# lst.append(1)
+# lst.append(2)
+# lst[0] = 3
+# print(lst, lst.get_length())
+
+# ==============================================================
+
+class MyMetaClass(type):
+    def __new__(mcs, name, bases, attr):
+        print("Создание нового объекта", name)
+        return super(MyMetaClass, mcs).__new__(mcs, name, bases, attr)
+
+    def __init__(cls, name, bases, attr):
+        print("Инициализация класса", name)
+        super(MyMetaClass, cls).__init__(name, bases, attr)
 
 
+class Student(metaclass=MyMetaClass):
+    def __init__(self, name):
+        self.name = name
+
+    def get_name(self):
+        return self.name
 
 
-
-
-
+stud = Student("Anna")
+print(stud.get_name())
+print(type(stud))
+print(type(Student))
 
